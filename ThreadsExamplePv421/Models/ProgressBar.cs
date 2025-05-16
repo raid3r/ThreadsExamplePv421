4dfa018f3d3bdcf7077aba18bc07e2a4ba793287
Vasyl Kovalov <raid3r81@gmail.com>
2025-05-16 20:34:57 +0300

    Добавлен класс ProgressBar для отображения прогресса
    В коде реализован класс `ProgressBar`, который включает свойства для ширины, значения и начальных координат. Добавлены методы `Clear` и `Draw` для очистки и отрисовки прогресс-бара в консоли, с использованием символов 'X' и '.' для визуализации текущего прогресса.

---
+++ ThreadsExamplePv421/Models/ProgressBar.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadsExamplePv421.Models;
+
+public class ProgressBar
+{
+    private int Width { get; set; } = 20;
+
+    public int Value { get; set; } = 0;
+    public int startX { get; set; } = 30;
+    public int startY { get; set; } = 7;
+
+
+    public void Clear()
+    {
+        lock (Console.Out)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.SetCursorPosition(startX, startY);
+            Console.Write("[" + new string(' ', Width) + "]");
+            Console.Write($"{0}%".PadLeft(5));
+        }
+    }
+
+    // [XXXXXXXXXXXXXXXXXXXX] 100%
+    public void Draw()
+    {
+        lock (Console.Out)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(startX, startY);
+            Console.Write("[");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write((float)Value / 5 > i ? "X" : ".");
+            }
+            Console.Write("]");
+            Console.Write($"{Value}%".PadLeft(5));
+        }
+    }
+}
